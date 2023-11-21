a7d7c457d135a7c3a118bcc0d86263dd3d6ecfa9
Ubuntu <ubuntu@ip-172-31-0-169.ap-south-1.compute.internal>
2023-11-21 10:40:05 +0000

    first project

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-doc
+
+# Check the Git branch
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+     ./build.sh
+     docker login -u nandini28 -p dckr_pat_ftPr0gp0_ytYTTJDUhH1KUXiTMk
+     docker tag myreactjsimg nandini28/dev
+     docker push nandini28/dev
+elif [[ $GIT_BRANCH == "origin/main" ]]; then 
+     ./build.sh
+     docker login -u nandini28 -p dckr_pat_ftPr0gp0_ytYTTJDUhH1KUXiTMk
+     docker tag myreactjsimg nandini28/prod
+     docker push nandini28/prod
+else
+    echo "deployment sucess"
+fi
+
+
+    
+
+
